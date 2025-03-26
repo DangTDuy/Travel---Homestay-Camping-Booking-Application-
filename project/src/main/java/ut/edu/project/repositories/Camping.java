8468f3e7e20db96a80a49e5d5c87a2ecd7eec6b5
Duy <dangthanhduyofficial@gmail.com>
2025-03-26 21:51:51 +0700

    cap nhat

--- project/src/main/java/ut/edu/project/repositories/Camping.java
+++ project/src/main/java/ut/edu/project/repositories/Camping.java
@@ -1,82 +1,31 @@
-package ut.edu.project.repositories;
+package ut.edu.project.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "campings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Camping {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String location;
-    private double price;
-    private String tentType;
-    private String ownerId;
+    private Double price;
+    private String description;
+    private int tentCapacity; // Số lượng lều có thể chứa
 
-    public Camping() {
-    }
+    @ElementCollection
+    private List<String> images; // Danh sách URL ảnh của khu cắm trại
 
-    public Camping(String id, String name, String location, double price, String tentType, String ownerId) {
-        this.id = id;
-        this.name = name;
-        this.location = location;
-        this.price = price;
-        this.tentType = tentType;
-        this.ownerId = ownerId;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getTentType() {
-        return tentType;
-    }
-
-    public void setTentType(String tentType) {
-        this.tentType = tentType;
-    }
-
-    public String getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(String ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    @Override
-    public String toString() {
-        return "Camping{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", location='" + location + '\'' +
-                ", price=" + price +
-                ", tentType='" + tentType + '\'' +
-                ", ownerId='" + ownerId + '\'' +
-                '}';
-    }
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner; // Chủ khu cắm trại
 }
