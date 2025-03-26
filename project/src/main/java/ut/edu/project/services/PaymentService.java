975bd0176e1422c7815db1a32c49c0f49eb00ce3
66TranMinhNhut <tranminhnhut200125@gmail.com>
2025-03-26 23:05:05 +0700

    PaymentService

---
+++ project/src/main/java/ut/edu/project/services/PaymentService.java
@@ -0,0 +1,12 @@
+package ut.edu.project.services;
+
+import ut.edu.project.models.Payment;
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentService {
+    Payment savePayment(Payment payment);
+    Optional<Payment> getPaymentById(Long id);
+    List<Payment> getAllPayments();
+    void deletePayment(Long id);
+}
