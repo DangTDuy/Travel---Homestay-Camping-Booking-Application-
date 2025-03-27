6aba330b3798f1e418a215b2729a7414e2b4eeac
Duy <dangthanhduyofficial@gmail.com>
2025-03-27 10:40:52 +0700

    cap nhat project

--- project/src/main/java/ut/edu/project/repositories/CampingRepository.java
+++
@@ -1,7 +0,0 @@
-package ut.edu.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ut.edu.project.models.Camping;
-
-public interface CampingRepository extends JpaRepository<Camping, Long> {
-}
