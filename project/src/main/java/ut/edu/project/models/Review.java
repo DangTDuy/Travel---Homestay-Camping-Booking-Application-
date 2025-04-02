0597d30e174b1941bd8cbbedf31552134e7c39a0
Khanh-Kiet <khanhkietbt2020@gmail.com>
2025-04-02 18:30:29 +0700

    Update Review.java

---
+++ project/src/main/java/ut/edu/project/models/Review.java
@@ -0,0 +1,23 @@
+package ut.edu.project.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String serviceType;
+    private Long serviceId;
+    private Integer rating;
+    private String comment;
+}
